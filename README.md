7762a4a0a61aada9641e8a8c1063ebc5a2689938
guillaumemolter <gmolter@hsph.harvard.edu>
2018-07-30 21:03:15 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# plugin-update-checker-private-github-loader
+# Plugin Update Checker Git loader
 Helper to configure https://github.com/YahnisElsts/plugin-update-checker for Github private repo.
+
+## How to use:
+
+## How to use:
+1. Then add this library to your plugin or theme `composer require guillaumemolter/plugin-update-checker-private-github-loader`
+2. Finally inside of your plugin or theme:
+
+```
+<?php
+
+$plugin_update_checker = new PrivateGithubSetup( 'superusername', 'repo-slug', 'XXXXXXXX' );
+// or
+$plugin_update_checker = new PrivateGithubSetup( 'superorgname', 'repo-slug', 'XXXXXXXX' );
+// or
+$plugin_update_checker = new PrivateGithubSetup( 'superorgname', 'repo-slug', 'XXXXXXXX', 'dev-branch' );
+$plugin_update_checker->init();
+
+?>
+
+
---
+++ src/PluginUpdaterAdmin/PrivateGithubSetup.php
@@ -0,0 +1,68 @@
+<?php
+namespace GuillaumeMolter\PluginUpdaterAdmin;
+
+/**
+ * PrivateGithubSetup Class.
+ */
+class PrivateGithubSetup {
+
+    /**
+     * The Github Token to fetch private repo data.
+     *
+     * @var string
+     */
+    private $github_token;
+
+    /**
+     * The Github username or org.
+     *
+     * @var string
+     */
+    public $github_user;
+
+    /**
+     * The Github repo slug.
+     *
+     * @var string
+     */
+    public $github_repo;
+
+    /**
+     * The Github branch.
+     *
+     * @var string
+     */
+    public $github_branch;
+
+    /**
+     * Class constructor.
+     */
+    public function __construct( string $github_user, string $github_repo, string $github_token, string $github_branch = 'master' ) {
+        $this->github_user   = $github_user;
+        $this->github_repo   = $github_repo;
+        $this->github_token  = $github_token;
+        $this->github_branch = $github_branch;
+    }
+
+    /**
+     * Plugin init function - register plugin hooks
+     *
+     * @return void
+     */
+    public function init() {
+        if ( ! empty( $this->github_token ) && ! empty( $this->github_user ) && ! empty( $this->github_repo ) && ! empty( $this->github_branch ) ) {
+            try {
+                $updater = Puc_v4_Factory::buildUpdateChecker(
+                    'https://github.com/' . $this->github_user . '/' . $this->github_repo,
+                    __FILE__,
+                    $this->github_repo
+                );
+                $updater->setAuthentication( $this->github_token );
+                $updater->setBranch( $this->github_branch );
+
+            } catch ( Exception $e ) {
+                echo 'PluginUpdaterAdmin caught an exception: ' .  $e->getMessage() . "\n";
+            }
+        }
+    }
+}
